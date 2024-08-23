7c3759560d11f22be03bf3a720fbdaf0069a3ab3
laurawarren88 <laura@enterpriseautomation.co.uk>
2024-08-23 11:04:13 +0100

    Final tweaks

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-alert('Hello World!');
+const express = require('express')
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'ejs')
+
+// Serve the HTML file
+app.get('/', (_req, res) => {
+    res.render('index'); 
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
